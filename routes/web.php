c65a0e98807a0a0d72dba94beea07dea49de2fa4
ThanhND1 <thanhnd1@smartosc.com>
2020-01-07 16:03:31 +0700

    Inital apps

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth.shop'])->name('home');
